12d3186b03abdf19957ce67b1b151b32148dc6c9
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-04-01 12:07:29 +0300

    Done Exercise001_RandomNumbersOfMatrix

---
+++ Exercise001_RandomNumbersOfMatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Clear();
+Console.Write("Enter the size of array: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+double[,] matrix = new double[size[0], size[1]];
+
+void InputMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+InputMatrix(matrix);
