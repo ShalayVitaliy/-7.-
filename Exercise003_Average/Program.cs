378f34ec61bd28916cba673ad4552fc4b7b15357
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-04-01 13:17:51 +0300

    Done Exercise003_Average

---
+++ Exercise003_Average/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+Console.Write("Enter the size of your matrix: ");
+int[] Size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] Matrix = new int[Size[0], Size[1]];
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 16);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+InputMatrix(Matrix);
+Console.WriteLine();
+
+void Average(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.Write($"{ sum / matrix.GetLength(0)} \t");
+    }
+}
+Average(Matrix);
